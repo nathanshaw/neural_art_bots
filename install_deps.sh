aff0aae860278dd90ce9335424daad210dcb11f9
Nathan Villicaña-Shaw <nathanshawsemail@gmail.com>
2023-02-24 16:39:46 -0800

    updated camera test, complete test, camera and dream test, and the dependency install script

--- install_deps.sh
+++ install_deps.sh
@@ -2,8 +2,8 @@
 # echo "enabling xcode build permissions"
 # sudo xcodebuild -license accept
 echo "brew isntalling portaudio as needed for pyaudio"
-brew install portaudio espeak
+brew install portaudio espeak ffmpeg
 echo "updating pip"
 pip install --upgrade pip
 echo "using pip to install python depencancies nltk"
-pip install --upgrade clang SpeechRecognition pyaudio nltk numpy matplotlib keras_cv keras-nlp tensorflow pycocotools opencv-python tensorflow_text
+pip install --upgrade clang SpeechRecognition pyaudio nltk numpy matplotlib keras_cv keras-nlp tensorflow pycocotools opencv-python tensorflow_text natsort tensorflow_io pydub
